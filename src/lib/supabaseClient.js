3667c706b57544fe64cfbc4c215795d1268804f6
Alexi118 <cuongnd118@gmail.com>
2025-02-03 23:58:32 +0700

    update dot env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv'
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://sqiudgsmqppkfovinfdp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxaXVkZ3NtcXBwa2ZvdmluZmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwODgxNzEsImV4cCI6MjAwOTY2NDE3MX0.Xa469GVCKZG6-tAin10i2yl-72Z1l1LvduZxql7Pnvc')
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
